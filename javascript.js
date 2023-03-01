6bab1f317349bf846ea3abbd6eb9413294b4eaf2
Oscar Soanes <oscarsoanes@gmail.com>
2023-03-01 00:16:31 +0000

    Added a game handler and reset functionality

--- javascript.js
+++ javascript.js
@@ -1,27 +1,61 @@
 const gameBoard = (() => {
-  const gameBoard = ["x", "", "o", "", "o", "", "x", "", ""];
-  const move = (move, position) => {
-    gameBoard[position] = move;
+  let gameBoard = ["", "", "", "", "", "", "", "", ""];
+  const move = (player, position) => {
+    gameBoard[position] = player;
     displayController(gameBoard);
   };
 
-  return {move};
+  const reset = () => {
+    gameBoard = ["", "", "", "", "", "", "", "", ""];
+    displayController(gameBoard);
+  };
+
+  return {move, reset};
 })();
 
-const displayController = (gameBoard) => {
+const displayController = (gameBoard, turn) => {
   const container = document.querySelector("#game-container");
+  container.innerHTML = "";
 
+  let index = 0;
   gameBoard.forEach((move) => {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     cell.textContent = move;
+    cell.setAttribute("index", index);
 
     container.appendChild(cell);
+    index++;
   });
 };
 
-const Player = (name) => {
-  return {name};
-};
+const game = (() => {
+  const player = (name) => {
+    return {name};
+  };
+  const player1 = player("x");
+  const player2 = player("o");
 
-gameBoard.move("x", 2);
+  let currentPlayer = player1;
+
+  const switchPlayer = () => {
+    if (currentPlayer === player1) {
+      console.log("true");
+      currentPlayer = player2;
+    } else {
+      console.log("false");
+      currentPlayer = player1;
+    }
+  };
+
+  const startGame = () => {
+    gameBoard.move("", 0);
+  };
+  return {startGame, player1, player2, switchPlayer, currentPlayer};
+})();
+
+game.startGame();
+
+// todo
+// const cells = document.querySelectorAll(".cell");
+// cells.forEach((cell) => cell.addEventListener("click", () => {}));
